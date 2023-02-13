a3c9b5a8fb16b889041a5a853b97ea578492970a
Wilson <wilson.guayanay@epn.edu.ec>
2023-02-13 12:07:57 -0500

    inicio

--- src/Inicio.java
+++ src/Inicio.java
@@ -1,5 +1,36 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Inicio {
-    private JPanel Inicio;
+public class Inicio extends  JFrame {
+    private JButton Admin;
+    private JButton cajeroButton;
+    private JLabel Buho;
+    private JPanel panel;
+
+    public Inicio(){
+        super("LOGIN");
+        setContentPane(panel);
+        Admin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame frame = new Inicio();
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.pack();
+                frame.setSize(640, 550);
+                frame.setLocationRelativeTo(null);
+                frame.setVisible(true);
+            }
+        });
+    }
 }
+
+
