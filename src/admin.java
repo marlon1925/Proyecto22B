8ae527dc9a8a4ca8bb1fe16dd2b98b4656cf165b
marlon <marlon.lalangui@epn.edu.ec>
2023-02-13 10:31:15 -0500

    creacion de los login para admin

---
+++ src/admin.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class admin {
+    private JPanel admin;
+    private JTextField cedulaAdmin;
+    private JPasswordField contraseñaAdmin;
+    private JButton botonInicioAdmin;
+}
