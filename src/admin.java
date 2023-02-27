4ee0bf541fea6c3744d04f6ead13dcfc40f146a7
Marlon <marlon.lalangui@epn.edu.ec>
2023-02-27 18:52:28 -0500

    base de datos

--- src/admin.java
+++ src/admin.java
@@ -12,22 +12,36 @@ public class admin extends Inicio{
     private JButton regresarButton;
 
     public admin (){
-        /*
+
         botonInicioAdmin.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-              try{
-                  con = getConection();
-                  st = con.prepareStatement("SELECT * FROM minimarket.administrador where CEDULA=" +
-                          cedulaAdmin.getText() + "AND CONTRASEÑA=" + contraseñaAdmin);
-                  while (rs.next()){
-                      System.out.println("si vale");
-                  }
-              }catch (HeadlessException | SQLException f){
-                  System.out.println(f);
-              }
+                con = getConection();
+
+                try {
+                    String usuario = cedulaAdmin.getText(); // obtener el usuario ingresado en el JTextField
+                    char[] password = contraseñaAdmin.getPassword(); // obtener la contraseña ingresada en el JPasswordField
+                    String passwordString = new String(password); // convertir la contraseña de char[] a String
+
+                    String query = "SELECT * FROM minimarket.administrador WHERE CEDULA=? AND CONTRASEÑA=?";
+                    ps = con.prepareStatement(query);
+                    ps.setString(1, usuario);
+                    ps.setString(2, passwordString);
+                    rs = ps.executeQuery();
+
+                    if (rs.next()) {
+                        // el usuario y la contraseña son correctos, hacer algo aquí
+
+                    } else {
+                        // el usuario y/o la contraseña son incorrectos, hacer algo aquí
+                        System.out.println("Usuario o contraseña incorrectos");
+                    }
+                } catch (SQLException f) {
+                    System.out.println(f);
+                }
             }
-        });*/
+        });
+
 
         regresarButton.addActionListener(new ActionListener() {
             @Override
