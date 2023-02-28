9e46cd5beff51de7ccbef2ee5c4bfad8ea5b4b35
WilsonG08 <wilson.guayanay@epn.edu.ec>
2023-02-27 19:55:21 -0500

    JPanel de Cajero

--- src/cajeroInicio.java
+++ src/cajeroInicio.java
@@ -1,2 +1,13 @@
+import javax.swing.*;
+
 public class cajeroInicio {
+    private JTextField txtCedulaCajero;
+    private JTextField txtContraseñaCajero;
+    private JButton regresarButton;
+    private JButton iniciarButton;
+    private JLabel LOGINCAJERO;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
